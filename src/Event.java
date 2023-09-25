b38500441ae5508de356e9945fd59c36f9441c06
SCooldude <sammanpandey7@gmail.com>
2023-09-25 13:14:56 -0400

    event done

--- src/Event.java
+++ src/Event.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 
 public class Event implements Comparable<Event> {
     private Date date;
@@ -20,7 +21,7 @@ public class Event implements Comparable<Event> {
     public boolean equals(Object obj) {
          if (this==obj) return true;
          Event event = (Event) obj;
-             return Objects.equals(eventDate, event.eventDate) &&
+             return Objects.equals(date, event.date) &&
                      Objects.equals(startTime, event.startTime) &&
                      Objects.equals(location, event.location);
 
@@ -32,7 +33,18 @@ public class Event implements Comparable<Event> {
         return "[Event Date: " + date + "] [Start: " + startTime + "] [End: " + startTime+duration + "] @" + location + " [Contact: " + contact + "]";
      }
 
-     private compareTo(){
+     public int compareTo(Event another){
+
+        int datecomp= this.date.compareTo(another.date);
+
+        if (datecomp==0){
+            int timeScomp= this.startTime.compareTo(another.startTime);
+            if (timeScomp==0){
+                return 0;
+            }
+        return timeScomp;
+        }
+     return datecomp;
 
      }
 }
