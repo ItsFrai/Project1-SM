07c540330d4ac592b39c8ba0cf3874139d335e8a
SCooldude <sammanpandey7@gmail.com>
2023-09-25 12:04:28 -0400

    Event 2 methods

--- src/Event.java
+++ src/Event.java
@@ -17,10 +17,25 @@ public class Event implements Comparable<Event> {
 
 
     @Override
-    public int compareTo(Event o) {
-        return 0;
-    }
-    string s= g;
+    public boolean equals(Object obj) {
+         if (this==obj) return true;
+         Event event = (Event) obj;
+             return Objects.equals(eventDate, event.eventDate) &&
+                     Objects.equals(startTime, event.startTime) &&
+                     Objects.equals(location, event.location);
+
+
+
+
+}
+     private String ToString(){
+        return "[Event Date: " + date + "] [Start: " + startTime + "] [End: " + startTime+duration + "] @" + location + " [Contact: " + contact + "]";
+     }
+
+     private compareTo(){
+
+
+     }
 }
 
 
